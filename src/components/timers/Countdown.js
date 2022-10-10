fa1df5d379713f4eccf7a8bc8c96c05c9d689e56
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-10-09 23:37:55 -0400

    initial formatting for counter display in H:M:S format

--- src/components/timers/Countdown.js
+++ src/components/timers/Countdown.js
@@ -3,8 +3,14 @@ import TimerBtn from "../../components/generic/TimerBtn";
 
 //const Countdown = ({ startVal }) => {
 const Countdown = () => {
-  let startVal = 5;
-  
+  //const startVal = 5;
+
+  const countHrs = 0,
+  	countMins = 2,
+  	countSecs = 5;
+
+  const startVal = countHrs * 60 * 60 + countMins * 60 + countSecs;
+
   const [count, setCount] = useState(startVal);
   const [isPaused, setPaused] = useState(false);
   const [isStopped, setStopped] = useState(true);
@@ -21,13 +27,18 @@ const Countdown = () => {
     return () => { if(t) { clearTimeout(t); }}
   }, [count, isPaused, isStopped]);
 
+  const timerHrsRemaining = Math.floor(count / (60 * 60));
+  const timerMinsRemaining = Math.floor((count - timerHrsRemaining * 60 * 60) / 60);
+  const timerSecsRemaining = count - timerHrsRemaining * 60 * 60 - timerMinsRemaining * 60;
+
   return (
     <div className="main-panel">
       <div className="display">Counter: {count}</div>
+      <div className="display">{timerHrsRemaining}:{timerMinsRemaining}:{timerSecsRemaining}</div>
       <TimerBtn label="Start" handler={() => { setCount(startVal); setStopped(false); setPaused(false); }}/>
-      <TimerBtn label="Stop" handler={() => { setStopped(true); }}/>
-      <TimerBtn label="Pause" handler={() => { setPaused(!isPaused); }}/><br/>
-      <TimerBtn label="Clear" handler={() => { setCount(0); setStopped(true); }}/>
+      <TimerBtn label="Stop" handler={() => setStopped(true) }/>
+      <TimerBtn label="Pause" handler={() => setPaused(!isPaused) }/><br/>
+      <TimerBtn label="Clear" handler={() => { setCount(0); setStopped(true) }}/>
       <TimerBtn label="Fast Forward" handler={() => { if(!isStopped) { setCount(0); }}}/>
     </div>
   );
--- src/components/timers/Stopwatch.js
+++ src/components/timers/Stopwatch.js
@@ -2,7 +2,11 @@ import { useState, useEffect, useRef } from 'react';
 import TimerBtn from "../../components/generic/TimerBtn";
 
 const Stopwatch = () => {
-  let countupVal = 5;
+  const countHrs = 5,
+  	countMins = 5,
+  	countSecs = 5;
+
+  const countupVal = countHrs * 60 * 60 + countMins * 60 + countSecs;
 
   const [count, setCount] = useState(0);
   const [isPaused, setPaused] = useState(false);
@@ -20,16 +24,19 @@ const Stopwatch = () => {
     return () => { if (t) { clearTimeout(t); } }
   }, [count, isPaused, isStopped]);
 
-  console.log('Rendering component, count is '+count);
+  const timerHrsRemaining = Math.floor(count / (60 * 60));
+  const timerMinsRemaining = Math.floor((count - timerHrsRemaining * 60 * 60) / 60);
+  const timerSecsRemaining = count - timerHrsRemaining * 60 * 60 - timerMinsRemaining * 60;
 
   return (
     <div className="main-panel">
-      <div className="display">Counter: {count}</div>
-      <button onClick={() => { setCount(0); setStopped(false); setPaused(false); }} >Start</button>
-      <button onClick={() => { setStopped(true); }} >Stop</button>
-      <button onClick={() => setPaused(!isPaused)}>Pause</button><br/>
-      <button onClick={() => { setCount(0); setStopped(true); }}>Clear</button>
-      <button onClick={() => { if(!isStopped) { setCount(countupVal); }}} >Fast Forward</button>
+    <div className="display">Counter: {count}</div>
+    <div className="display">{timerHrsRemaining}:{timerMinsRemaining}:{timerSecsRemaining}</div>
+	<TimerBtn label="Start" handler={() => { setCount(0); setStopped(false); setPaused(false); }}/>
+	<TimerBtn label="Stop" handler={() => setStopped(true)} />
+	<TimerBtn label="Pause" handler={() => setPaused(!isPaused)}/>
+	<TimerBtn label="Clear" handler={() => { setCount(0); setStopped(true); }}/>
+	<TimerBtn label="Fast Forward<" handler={() => { if(!isStopped) { setCount(countupVal); }}}/>
     </div>
   );
 }
