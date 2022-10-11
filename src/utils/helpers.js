4a0ca9e524a0eb13558260eba29eece4469f9cd1
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-10-10 22:52:24 -0400

    added helper functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,15 @@
 // Add helpers here. This is usually code that is just JS and not React code. Example: write a function that
 // calculates number of minutes when passed in seconds. Things of this nature that you don't want to copy/paste
 // everywhere.
+
+export const incrementHelper = (val, upLimit=99) => {
+  if (val === upLimit) { return val; }
+
+  return val+1;
+}
+
+export const decrementHelper = (val, downLimit=0) => {
+if (val == downLimit) { return val; }
+
+return val-1;
+}
