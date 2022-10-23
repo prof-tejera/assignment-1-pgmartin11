f844bb73d9079f7b5842a1433d5785b0ff14c529
Peter Martin <pgmartin@peters-mbp.lan>
2022-10-23 18:17:29 -0400

    added generic components to documentation

--- src/views/DocumentationView.js
+++ src/views/DocumentationView.js
@@ -4,6 +4,10 @@ import styled from "styled-components";
 import DocumentComponent from "../components/documentation/DocumentComponent";
 
 import Loading from "../components/generic/Loading";
+import DisplayTime from "../components/generic/DisplayTime";
+import DisplayRound from "../components/generic/DisplayRound";
+import TimerBtn from "../components/generic/TimerBtn";
+
 
 const Container = styled.div`
   display: flex;
@@ -32,6 +36,60 @@ const Documentation = () => {
               description: "Changes the size of the loading spinner",
               type: "string",
               defaultValue: "medium",
+            },
+          ]}
+        />
+        <DocumentComponent
+          title="Display counter value in hours, minutes, seconds format"
+          component={<DisplayTime label="Counter" count={363} />}
+          propDocs={[
+            {
+              prop: "label",
+              description: "Label for the counter metric",
+              type: "string",
+              defaultValue: "N/A",
+            },
+            {
+              prop: "count",
+              description: "Counter time in seconds",
+              type: "integer",
+              defaultValue: "N/A",
+            },
+          ]}
+        />
+        <DocumentComponent
+          title="Display round number"
+          component={<DisplayRound round={1}/>}
+          propDocs={[
+            {
+              prop: "round",
+              description: "Represents the current round",
+              type: "integer",
+              defaultValue: "N/A",
+            },
+          ]}
+        />
+        <DocumentComponent 
+          title="Timer button"
+          component={<TimerBtn label="Resume" handler={f=>f} disabled={false}/>} 
+          propDocs={[
+            {
+              prop: "label",
+              description: "Button label",
+              type: "string",
+              defaultValue: "N/A",
+            },
+            {
+              prop: "handler",
+              description: "Button click handler",
+              type: "function",
+              defaultValue: "N/A",
+            },
+            {
+              prop: "disabled",
+              description: "Represents the button state, enabled or disabled",
+              type: "boolean",
+              defaultValue: "false",
             },
           ]}
         />
